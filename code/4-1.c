78bc4bdaa1a30a25df3375f71b030bea65480ab1
Neo6424 <129708429+Neo6424@users.noreply.github.com>
2026-03-16 00:07:20 +0900

    Update 4-1.c

--- code/4-1.c
+++ code/4-1.c
@@ -4,7 +4,7 @@ int main()
 {
     int num1,num2;
     char op;
-    scanf("%d %d %s", &num1, &num2, &op);
+    scanf("%d %d %c", &num1, &num2, &op);
     if (op == '+') printf("%d\n", num1 + num2);
     else if (op == '-') printf("%d\n", num1 - num2);
     else if (op == '*') printf("%d\n", num1 * num2);
