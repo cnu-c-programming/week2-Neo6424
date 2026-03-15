24851a893fe141e4cc284459f7a7a045022e5f46
Neo6424 <129708429+Neo6424@users.noreply.github.com>
2026-03-16 00:22:57 +0900

    Update 4-3.c

--- code/4-3.c
+++ code/4-3.c
@@ -1,7 +1,26 @@
 #include <stdio.h>
+#include <math.h>
 
 int main()
 {
+    int n;
+    int divisor = 0;
+
+    scanf("%d", &n);
+    for(int i =2; i <= sqrt(n); i++){
+        int test = n % i;
+        if(test == 0){
+            divisor++;
+        }
+        if(divisor > 1){
+            printf("false");
+            break;
+        }
+
+    }
+    if(divisor == 0){
+        printf("true");
+    }
 
     return 0;
 }
